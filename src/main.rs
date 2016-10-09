2c89a331560f6f017046d56dda07d140b6c664f7
Kirk Petersen <kirk.petersen@gmail.com>
2016-10-09 16:30:01 -0700

    testing out image manipulation

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
 extern crate image;
 
 use std::path::Path;
+use std::fs::File;
 
 use image::GenericImage;
 
 fn main() {
-    let im = image::open(&Path::new("Lenna.png")).unwrap();
+    // This is a "image::DynamicImage", which I guess wraps
+    // the different images that can result in parsing an image
+    // file?
 
-    println!("dimensions: {:?}", im.dimensions())
+    println!("opening lenna image");
+
+    let mut img = image::open(&Path::new("Lenna.png")).unwrap();
+
+    println!("inverting image");
+
+    img.invert();
+
+    println!("converting to luma");
+
+    let imgbuf = img.to_rgb();
+
+    println!("writing out inverted image");
+
+    let ref mut fout = File::create(&Path::new("test-invert.png")).unwrap();
+    let _ = image::ImageRgb8(imgbuf).save(fout, image::PNG);
 }
